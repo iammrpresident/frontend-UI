c2a9d2420d39dea9f813c4dbd1ce5fd1a8249c42
Aniekan Akpan <aniekanbtc@gmail.com>
2023-12-04 12:20:55 +0100

    Completed landing page design

---
+++ frontend/src/components/Features.vue
@@ -0,0 +1,40 @@
+<template>
+<section class="bg-white dark:bg-gray-900">
+  <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+      <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">📌 Key Features</h2>
+      </div>
+      <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+          <div>
+
+              <h3 class="mb-2 text-xl font-bold dark:text-white">✅ Simple & Intuitive</h3>
+              <p class="text-gray-500 dark:text-gray-400">Our user-friendly interface ensures a seamless experience, allowing you to focus on what matters most - your tasks.</p>
+          </div>
+          <div>
+
+              <h3 class="mb-2 text-xl font-bold dark:text-white">📅 Smart Scheduling</h3>
+              <p class="text-gray-500 dark:text-gray-400">Stay on top of your game with customizable reminders. Receive notifications to keep you informed and in control.</p>
+          </div>
+          <div>
+
+              <h3 class="mb-2 text-xl font-bold dark:text-white">🔗 Collaborative Workspaces</h3>
+              <p class="text-gray-500 dark:text-gray-400">Foster teamwork by creating collaborative workspaces. Share projects, assign tasks, and achieve goals together.</p>
+          </div>
+          <div>
+
+              <h3 class="mb-2 text-xl font-bold dark:text-white">⚙️ Cross-Platform Sync</h3>
+              <p class="text-gray-500 dark:text-gray-400">Seamless synchronization across devices ensures that you're always in the loop, whether on your desktop, tablet, or smartphone.</p>
+          </div>
+          <div>
+
+              <h3 class="mb-2 text-xl font-bold dark:text-white">🌐 Accessible Anywhere</h3>
+              <p class="text-gray-500 dark:text-gray-400">Access your todos from anywhere in the world with our cloud-based solution. Your tasks, your way, wherever you go.</p>
+          </div>
+          <div>
+              <h3 class="mb-2 text-xl font-bold dark:text-white">🛡️ Secure & Private</h3>
+              <p class="text-gray-500 dark:text-gray-400"> Your data is important. Rest easy with our robust security measures and privacy safeguards</p>
+          </div>
+      </div>
+  </div>
+</section>
+</template>
---
+++ frontend/src/components/Hero.vue
@@ -0,0 +1,15 @@
+<template>
+<section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 ">
+        <a href="#" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+        </a>
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">🌟Your Ultimate Todo Companion🌟</h1>
+        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Empower your productivity with our intuitive todo application designed to streamline your daily tasks and supercharge your efficiency. Say goodbye to chaos and hello to organized success! 🚀</p>
+        <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                Get Started
+            </a> 
+        </div> 
+    </div>
+</section>
+</template>
